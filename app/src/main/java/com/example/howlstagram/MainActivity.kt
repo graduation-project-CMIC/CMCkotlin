4171e2a87e636636e8796ebe5be84d4b0491a7f6
gudwnsrh@gmail.com <gudwnsrh@gmail.com>
2020-05-01 21:30:19 +0900

    2020.05.01 kotlin save

---
+++ app/src/main/java/com/example/howlstagram/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.howlstagram
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.example.howlstagram.navigation.AlarmFragment
+import com.example.howlstagram.navigation.DetailViewFragment
+import com.example.howlstagram.navigation.GridFragment
+import com.example.howlstagram.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.action_home->{
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true;
+            }
+            R.id.action_favorite_alarm->{
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true;
+            }
+            R.id.action_search->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true;
+            }
+            R.id.action_add_photo->{
+               return true;
+            }
+            R.id.action_account->{
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true;
+            }
+        }
+        return false;
+    }
+}
